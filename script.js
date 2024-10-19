89216a95c56a7a0cc069ca06346f83cd509bb6c7
yashkumar84 <166642691+yashkumar84@users.noreply.github.com>
2024-10-19 12:39:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 window.addEventListener("load", init);
 
 function init() {
-  getData();
+  const input = document.querySelector(".input");
+  document
+    .querySelector(".btn")
+    .addEventListener("click", () => getData(input.value));
 }
 
-async function getData() {
+async function getData(value) {
   const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=f635e071d50e448e97c94737241310&q=dehradun"
+    `https://api.weatherapi.com/v1/current.json?key=f635e071d50e448e97c94737241310&q=${value}`
   );
   const data = await response.json();
   console.log("The data is ", data);
@@ -21,7 +24,8 @@ function printData(data) {
     data.current.condition.text
   );
 
-  const div = document.querySelector(".container");
+  const div = document.querySelector(".information");
+  div.innerHTML = null;
   const h4 = document.createElement("h4");
   h4.innerText = data.location.name;
   div.appendChild(h4);
